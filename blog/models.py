6aec61d904a6717cda527c8180a077dd37726ef4
unknown <paul.mouzas@gmail.com>
2014-09-11 11:37:58 -0400

    add users

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django import forms
+from django.contrib.auth.models import User
 
 class Entry(models.Model):
+    author = models.ForeignKey(User, blank=True)
     title = models.CharField(max_length=50)
     post = models.TextField(max_length=2000)
     pub_date = models.DateTimeField('date published')
@@ -10,9 +12,11 @@ class Entry(models.Model):
         return self.title
     
 class Comment(models.Model):
+    author = models.ForeignKey(User, blank=True)
     entry = models.ForeignKey(Entry)
     text = models.CharField(max_length=50)
     pub_date = models.DateTimeField('date published')
     
     def __unicode__(self):
         return self.text
+        
