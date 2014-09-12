127b7c415d059f9df397486d0d226148c34b8509
unknown <paul.mouzas@gmail.com>
2014-09-12 11:23:48 -0400

    add logout link

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from blog import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<entry_id>\d+)/$', views.detail, name='detail')
+    url(r'^(?P<entry_id>\d+)/$', views.detail, name='detail'),
+    url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': 'index'}),
 )
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from blog.models import Entry
+from django.contrib.auth import logout
 
 def index(request):
     latest_entry_list = Entry.objects.order_by('-pub_date')[:5]
@@ -13,3 +14,4 @@ def index(request):
     
 def detail(request, entry_id):
     return HttpResponse("You're looking at blog post number %s." % entry_id)
+    
