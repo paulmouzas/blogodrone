e8ed0db0362a1d23efdba81d5071658d1e2f1f7f
Paul Mouzas <paul.mouzas@gmail.com>
2014-10-26 16:49:03 -0400

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -66,6 +66,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
+DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
