76989c10eb2f618a7e90432d0a0c7ab4b10feedc
Paul Mouzas <paul.mouzas@gmail.com>
2014-10-26 14:12:56 -0400

    add virtualenv

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,4 @@
+from blog import views
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -7,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.index),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
 )
