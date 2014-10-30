d5e03958e188077594f4ecdecf9693680163e61f
Paul Mouzas <paul.mouzas@gmail.com>
2014-10-30 11:55:24 -0400

    modify wsgi

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from blog.models import Entry
+
+class EntryTestCase(TestCase):
+    def setUp(self):
+        self.u1 = User.objects.create(username="user1")
+        self.e1 = Entry.objects.create(author=self.u1, title="testing", 
+                                       post="this is a post", pub_date=datetime.now())
+
+    def testA(self):
+        self.assertEqual(self.u1.username, "user1")
+        self.assertEqual(self.e1.author, self.u1) 
+
+    def tearDown(self):
+        self.u1.delete()
+        self.e1.delete()
