cbe1b2c36a8c03c98794203d237e3fbf21939baa
Paul Mouzas <paul.mouzas@gmail.com>
2014-11-10 16:29:02 -0500

    add tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,12 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.utils import timezone
+from models import SignupForm, Entry
 
 from blog.models import Entry
 
 class EntryTestCase(TestCase):
     def setUp(self):
-        self.u1 = User.objects.create(username="user1")
+        self.u1 = User.objects.create_user(username="user1", email="user1@example.com", password="foobar")
         self.e1 = Entry.objects.create(author=self.u1, title="testing", 
-                                       post="this is a post", pub_date=datetime.now())
+                                       post="this is a post", pub_date=timezone.now())
 
     def testA(self):
         self.assertEqual(self.u1.username, "user1")
