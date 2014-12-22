e4234298eb7ae1d9947714641c981c3cf85cdb0c
paul <paul.mouzas@gmail.com>
2014-12-21 22:49:52 -0500

    edit settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,9 +1,3 @@
-ngo settings for mysite project.
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import django.conf.global_settings as DEFAULT_SETTINGS
