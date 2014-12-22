57c6909268262648b806f1174934eaee95175497
paul <paul.mouzas@gmail.com>
2014-12-21 23:23:36 -0500

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'jjd29d)x&(ja7xx1lc5y0-a8$ox&8b=l@sbxewb#r8&u6t=w0w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 TEMPLATE_DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # Application definition
 INSTALLED_APPS = (
 'django.contrib.admin',
