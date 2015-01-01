b0591b8d2fe62be1441ee99f91291632a6afc053
paul <paul.mouzas@gmail.com>
2015-01-01 17:56:47 -0500

    static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ STATICFILES_FINDERS = (
 # Static asset configuration
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
