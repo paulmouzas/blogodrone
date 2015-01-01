2637c04638ad7a94979841cc99cb339564ad66c3
paul <paul.mouzas@gmail.com>
2015-01-01 17:09:11 -0500

    add whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,6 +82,8 @@ STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,4 +8,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
